11af359f64301c48444fcb8ad3624a890db380cc
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-07-20 20:55:03 -0300

    SQL Generator: selectGenerator passing all tests

--- src/sqlGenerator/SQLGenerator.java
+++ src/sqlGenerator/SQLGenerator.java
@@ -1,10 +1,28 @@
 package sqlGenerator;
 
+import java.util.ArrayList;
+
 public class SQLGenerator {
 
-    public String generate(String table) {
+    public String fromTableNameToSelectQuery(String table) {
 
         String query = "SELECT * FROM " + table + ";";
+
+        return query;
+    }
+
+    public String generateSelectQuery(ArrayList<String> columns, String table) {
+
+        String query = "SELECT ";
+
+        if(columns.isEmpty())
+            query += "*";
+        else {
+            for(int i = 0; i < columns.size(); i++)
+                query += columns.get(i) + " ";
+        }
+
+        query += "FROM " + table + ";";
 
         return query;
     }
--- test/SQLGeneratorTest.java
+++ test/SQLGeneratorTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import sqlGenerator.SQLGenerator;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class SQLGeneratorTest {
@@ -10,9 +12,22 @@ public class SQLGeneratorTest {
         SQLGenerator generator = new SQLGenerator();
 
         String table = "a";
-        String result = generator.generate(table);
+        String result = generator.fromTableNameToSelectQuery(table);
 
         assertEquals("SELECT * FROM " + table + ";", result);
+    }
+
+    @Test
+    public void givenAColumnAndTableShouldReturnSelectQuery() throws Exception {
+        SQLGenerator generator = new SQLGenerator();
+        String table = "table";
+        ArrayList<String> columns = new ArrayList<String>();
+        columns.add("column");
+
+        String result = generator.generateSelectQuery(columns, table);
+
+        assertEquals("SELECT column FROM " + table + ";", result);
+
 
     }
 }

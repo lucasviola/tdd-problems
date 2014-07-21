c5bf071ee55c33ca78d598d36d510807f90cf799
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-07-20 21:31:43 -0300

    SQL Generator: SELECT Query done. Still yet to refactor.

--- src/sqlGenerator/SQLGenerator.java
+++ src/sqlGenerator/SQLGenerator.java
@@ -18,11 +18,71 @@ public class SQLGenerator {
         if(columns.isEmpty())
             query += "*";
         else {
-            for(int i = 0; i < columns.size(); i++)
-                query += columns.get(i) + " ";
+            for(int i = 0; i < columns.size(); i++){
+
+                if(i == columns.size()-1)
+                    query += columns.get(i) + " ";
+                else
+                    query += columns.get(i) + ", ";
+            }
         }
 
         query += "FROM " + table + ";";
+
+        return query;
+    }
+
+    public String generateSelectQuery(ArrayList<String> columns, String table, String clause, String columnMatch ){
+
+        String query = "SELECT ";
+
+        if(columns.isEmpty())
+            query += "*";
+        else {
+            for(int i = 0; i < columns.size(); i++){
+
+                if(i == columns.size()-1)
+                    query += columns.get(i) + " ";
+                else
+                    query += columns.get(i) + ", ";
+            }
+        }
+
+        query += "FROM " + table + " ";
+        query += clause + " ";
+        query += columnMatch + ";";
+
+        return query;
+    }
+
+    public String generateSelectQuery(ArrayList<String> columns, String table, String clause, ArrayList<String> matches) {
+
+        String query = "SELECT ";
+
+        if(columns.isEmpty())
+            query += "*";
+        else {
+            for(int i = 0; i < columns.size(); i++){
+
+                if(i == columns.size()-1)
+                    query += columns.get(i) + " ";
+                else
+                    query += columns.get(i) + ", ";
+            }
+        }
+
+        query += "FROM " + table + " ";
+        query += clause + " ";
+        query += matches.get(0);
+        if(matches.size() > 1){
+
+            for(int i = 1; i < matches.size(); i++){
+                query += " AND ";
+                query += matches.get(i);
+            }
+        }
+
+        query += ";";
 
         return query;
     }
--- test/SQLGeneratorTest.java
+++ test/SQLGeneratorTest.java
@@ -1,8 +1,10 @@
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.Test;
 import sqlGenerator.SQLGenerator;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SQLGeneratorTest {
@@ -27,6 +29,65 @@ public class SQLGeneratorTest {
         String result = generator.generateSelectQuery(columns, table);
 
         assertEquals("SELECT column FROM " + table + ";", result);
+    }
+
+    @Test
+    public void givenTwoColumnsAndATableShouldReturnSelectQuery() throws Exception {
+        SQLGenerator generator = new SQLGenerator();
+        String table = "table";
+        ArrayList<String> columns = new ArrayList<String>();
+        columns.add("firstColumn");
+        columns.add("secondColumn");
+
+        String result = generator.generateSelectQuery(columns, table);
+
+        assertEquals("SELECT firstColumn, secondColumn FROM table;", result);
+    }
+
+    @Test
+    public void givenThreeColumnsAndATableShouldReturnSelectQuery() throws Exception {
+        SQLGenerator generator = new SQLGenerator();
+        String table = "table";
+        ArrayList<String> columns = new ArrayList<String>();
+        columns.add("firstColumn");
+        columns.add("secondColumn");
+        columns.add("thirdColumn");
+
+        String result = generator.generateSelectQuery(columns, table);
+
+        assertEquals("SELECT firstColumn, secondColumn, thirdColumn FROM table;", result);
+
+    }
+
+    @Test
+    public void givenOneColumnAndOneTableAndWhereClauseShouldReturnSelectQuery() throws Exception {
+        SQLGenerator generator = new SQLGenerator();
+        String table = "table";
+        ArrayList<String> columns = new ArrayList<String>();
+        columns.add("column");
+        String clause = "WHERE";
+        String columnMatch = "id=1";
+
+        String result = generator.generateSelectQuery(columns, table, clause, columnMatch);
+
+        assertEquals("SELECT column FROM table WHERE id=1;", result);
+
+    }
+
+    @Test
+    public void givenOneColumnAndOneTableAndWhereClauseAndAndClauseReturnSelectQuery() throws Exception {
+        SQLGenerator generator = new SQLGenerator();
+        String table = "table";
+        ArrayList<String> columns = new ArrayList<String>();
+        columns.add("column");
+        String clause = "WHERE";
+        ArrayList<String> matches = new ArrayList<String>();
+        matches.add("id=1");
+        matches.add("age=20");
+
+        String result = generator.generateSelectQuery(columns, table, clause, matches);
+
+        assertEquals("SELECT column FROM table WHERE id=1 AND age=20;", result);
 
 
     }

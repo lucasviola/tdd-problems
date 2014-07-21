fbd82b20705763173fbfb7c5b3b03f1813407ede
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-07-21 18:35:34 -0300

    SQL Generator: INSERT 100%

--- src/sqlGenerator/SQLGenerator.java
+++ src/sqlGenerator/SQLGenerator.java
@@ -1,5 +1,7 @@
 package sqlGenerator;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
 import java.util.ArrayList;
 
 public class SQLGenerator {
@@ -60,6 +62,36 @@ public class SQLGenerator {
         }
 
         query += ";";
+
+        return query;
+    }
+
+    public String generateInsertQuery(String table, ArrayList<String> values) {
+
+        String query = "INSERT INTO " + table + " VALUES ";
+        query += "(";
+
+        for(int i = 0; i < values.size(); i++){
+
+            if(values.get(i).matches("^[a-zA-Z._@!#$%&*]+$")){
+
+                if(i != values.size()-1)
+                    query += "\"" + values.get(i) + "\", ";
+                else
+                    query += "\"" + values.get(i) + "\"";
+            }
+
+            else{
+                if(i != values.size()-1)
+                    query += values.get(i) + ", ";
+                else
+                    query += values.get(i);
+            }
+
+
+        }
+
+        query += ");";
 
         return query;
     }
--- test/SQLGeneratorTest.java
+++ test/SQLGeneratorTest.java
@@ -82,7 +82,71 @@ public class SQLGeneratorTest {
         String result = generator.generateSelectQuery(columns, table, clause, columnMatch);
 
         assertEquals("SELECT column FROM table WHERE id=1 AND age=20;", result);
+    }
 
+    @Test
+    public void givenTwoValuesShouldInsertIntoTable() throws Exception {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("lucas");
+        values.add("24");
 
+        String result = generator.generateInsertQuery(table, values);
+
+        assertEquals("INSERT INTO table VALUES (\"lucas\", 24);", result);
+    }
+
+    @Test
+    public void givenTwoStringsShouldInsertIntoTable() throws Exception {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("lucas");
+        values.add("Brasil");
+
+        String result = generator.generateInsertQuery(table, values);
+
+        assertEquals("INSERT INTO table VALUES (\"lucas\", \"Brasil\");", result);
+
+    }
+
+    @Test
+    public void givenOneSingleValueShouldInsertIntoTable() throws Exception {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("24");
+
+        String result = generator.generateInsertQuery(table, values);
+
+        assertEquals("INSERT INTO table VALUES (24);", result);
+    }
+
+    @Test
+    public void givenTwoIntegersShouldInsertIntoTable() throws Exception {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("1");
+        values.add("24");
+
+        String result = generator.generateInsertQuery(table, values);
+
+        assertEquals("INSERT INTO table VALUES (1, 24);", result);
+    }
+
+    @Test
+    public void givenThreeStringsShouldInsertIntoTable() throws Exception {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("lucas");
+        values.add("mail@mailto.com");
+        values.add("Brasil");
+
+        String result = generator.generateInsertQuery(table, values);
+
+        assertEquals("INSERT INTO table VALUES (\"lucas\", \"mail@mailto.com\", \"Brasil\");", result);
+    }
+
+    @Test
+    public void givenASetOfSpecialSymbolsShouldValidateAsAString() throws Exception {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("!@#$%*");
+
+        String result = generator.generateInsertQuery(table, values);
+
+        assertEquals("INSERT INTO table VALUES (\"!@#$%*\");", result);
     }
 }

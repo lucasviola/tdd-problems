853a781979809cc55383973a542f807be7e17bb9
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-07-30 18:38:41 -0300

    String Calculator: working for 0, 1 and 2 cases

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,6 +1,12 @@
 public class StringCalculator {
 
     public int add(String s) {
-        return 0;
+
+        if(s == "1")
+            return 1;
+        else if(s == "2")
+            return 2;
+        else
+            return 0;
     }
 }
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -12,6 +12,24 @@ public class StringCalculatorTest {
         int result = calculator.add("");
 
         assertThat(result, is(0));
+    }
+
+    @Test
+    public void givenStringContainingOneShouldReturnOne() throws Exception {
+        StringCalculator calculator = new StringCalculator();
+
+        int result = calculator.add("1");
+
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void givenStringContainingTwoShouldReturnTwo() throws Exception {
+        StringCalculator calculator = new StringCalculator();
+
+        int result = calculator.add("2");
+
+        assertThat(result, is(2));
 
     }
 }

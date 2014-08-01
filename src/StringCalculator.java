dff72b0600e2fb4d113b377a135de268b491ef34
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-07-31 22:08:36 -0300

    String Calculator: Refactored

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,12 +4,7 @@ public class StringCalculator {
 
     public int add(String s) {
         String[] someName =  s.split(",");
-        ArrayList<String> tokens = new ArrayList<String>();
         int result = 0;
-
-//        for(int i = 0; i < someName.length; i++){
-//            tokens.add(someName[i]);
-//        }
 
         for(int i = 0; i < someName.length; i++){
             if(someName[i].equals("1"))
